12ef06dbcf42781e95114d6bfc5c2461ccbdbaa9
Harsh Chokshi <hchokshi@users.noreply.github.com>
2026-05-25 18:57:56 -0700

    maintainers: add hchokshi

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -10626,6 +10626,11 @@
     githubId = 147689;
     name = "Hans-Christian Esperer";
   };
+  hchokshi = {
+    github = "hchokshi";
+    githubId = 10136407;
+    name = "Harsh Chokshi";
+  };
   hdhog = {
     name = "Serg Larchenko";
     email = "hdhog@hdhog.ru";
